d3169038de842129dc799b96917a69ddd640fa31
krithika1988 <krithibaluak1988@gmail.com>
2025-07-02 20:44:23 +0800

    Add files via upload
    changes made to mocks.py and no_pi.py for packing bay updation.

--- samples_and_snippets/mocks.py
+++ samples_and_snippets/mocks.py
@@ -2,24 +2,55 @@ from interfaces import CarparkSensorListener
 from interfaces import CarparkDataProvider
 import time
 
-class MockCarparkManager(CarparkSensorListener,CarparkDataProvider):
+class BayStatusTracker:
+    """Tracks the status of each parking bay (occupied or available)."""
+    def __init__(self, num_bays: int):
+        self.bays = [False] * num_bays  # False = available, True = occupied
+
+    def occupy_bay(self, bay_index: int):
+        if 0 <= bay_index < len(self.bays):
+            self.bays[bay_index] = True
+
+    def free_bay(self, bay_index: int):
+        if 0 <= bay_index < len(self.bays):
+            self.bays[bay_index] = False
+
+    def available_bays(self) -> int:
+        return self.bays.count(False)
+
+    def occupied_bays(self) -> int:
+        return self.bays.count(True)
+
+class MockCarparkManager(CarparkDataProvider, CarparkSensorListener):
+    def __init__(self):
+        self.tracker = BayStatusTracker(num_bays=1000)
+        self._temperature = 22
+        self._current_time = time.localtime()
+
     @property
     def available_spaces(self):
-        return 1000
+        return self.tracker.available_bays()
 
     @property
     def temperature(self):
-        return 1000
+        return self._temperature
 
     @property
     def current_time(self):
-        return time.localtime()
+        return self._current_time
 
-    def incoming_car(self,license_plate):
-        print('Car in! ' + license_plate)
+    def incoming_car(self, license_plate):
+        for i, occupied in enumerate(self.tracker.bays):
+            if not occupied:
+                self.tracker.occupy_bay(i)
+                break
 
-    def outgoing_car(self,license_plate):
-        print('Car out! ' + license_plate)
+    def outgoing_car(self, license_plate):
+        for i, occupied in enumerate(self.tracker.bays):
+            if occupied:
+                self.tracker.free_bay(i)
+                break
 
-    def temperature_reading(self,reading):
-        print(f'temperature is {reading}')
+    def temperature_reading(self, temp):
+        self._temperature = temp
+        self._current_time = time.localtime()
